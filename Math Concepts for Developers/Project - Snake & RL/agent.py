32a4be68660a570315060a4edf2f5db5b20c1b8e
Vladi <vladi85264@abv.bg>
2025-05-13 22:25:06 +0300

    Initial game and agent setup

---
+++ Math Concepts for Developers/Project - Snake & RL/agent.py
@@ -0,0 +1,38 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LEARNING_RATE = 0.001
+
+class Agent:
+    def __init__(self):
+        self.number_of_games = 0
+        self.epsilon = 0 # control randomness
+        self.gamma = 0 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        # TODO: model, trainer
+
+    def get_state(self, game):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass
+
+def train():
+    pass
+
+if __name__ == '__main__':
+    train()
