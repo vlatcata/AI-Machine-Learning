105b14a555c5fd1256dcf989bb755b370d8b4fc7
Vladi <vladi85264@abv.bg>
2025-05-14 23:16:20 +0300

    Implemented plotting

--- Math Concepts for Developers/Project - Snake & RL/agent.py
+++ Math Concepts for Developers/Project - Snake & RL/agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -133,7 +134,11 @@ def train():
 
             print('Game', agent.number_of_games, 'Score', score, 'Record', max_score)
 
-            # TODO: plot
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.number_of_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores) # plot the scores
 
 
 if __name__ == '__main__':
