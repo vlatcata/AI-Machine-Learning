dd9a6b99a3b296c95e904d9ad7fc88b3d23825bf
Vladi <vladi85264@abv.bg>
2025-05-26 15:50:19 +0300

    some changes

--- Math Concepts for Developers/Project - Snake & RL/helper.py
+++ Math Concepts for Developers/Project - Snake & RL/helper.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 from IPython import display
 
+# Enable interactive mode
 plt.ion() # To plot interactively
 
 def plot(scores, mean_scores):
     display.clear_output(wait=True)
     display.display(plt.gcf()) # Get current figure
     plt.clf() # Clear the current figure
+
     plt.title('Training...')
     plt.xlabel('Number of games')
     plt.ylabel('Score')
-    plt.plot(scores)
-    plt.plot(mean_scores)
+
+    plt.plot(scores, label='Scores', color='blue')
+    plt.plot(mean_scores, label='Mean Scores', color='orange')
     plt.ylim(ymin=0)
     plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
+
+    plt.legend(loc='upper left')
     plt.show(block=False)
+
     plt.pause(0.1) # Pause to update the plot
