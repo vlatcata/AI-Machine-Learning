91458f42a6d50de0ed0a47af45543de026bc2cc9
Vladi <vladi85264@abv.bg>
2025-08-12 21:15:22 +0300

    .

---
+++ Data Science/07. Data-Science-Project-Architecture-Lab/src/DataProcessor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# The first notebook is Data Exploration, nothing to extract here that is needed.
+
+# Third notebook is Data Analysis so nothing to extract here either.
+
+# From notebook 2 I will extract the preprocessing steps into one function.
+def preprocess_data(X: pd.DataFrame):
+    pass
+
+# From notebook 4 I will extract everything into one function and maybe rewrite some code a little to follow some good practices.
+def manipulate_features(X: pd.DataFrame):
+    pass
