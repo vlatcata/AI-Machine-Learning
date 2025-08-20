2d0a212b8577c07203873d5014905e0df2f8d2f2
Vladi <vladi85264@abv.bg>
2025-08-21 00:26:47 +0300

    Fixing the broken header in  the dataset automatically

--- Data Science/Project - Best Video Game Analysis/src/utils.py
+++ Data Science/Project - Best Video Game Analysis/src/utils.py
@@ -29,3 +29,25 @@ def range_to_average(x):
         return int((start + end) / 2)
     except ValueError:
         return None
+    
+def read_games_csv_with_broken_header(path):
+    """Read the specific Games CSV file with a broken header.
+
+    Args:
+        path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The loaded DataFrame with fixed Headers.
+    """
+    with open(path, "r", encoding="utf-8") as f:
+        broken_header = f.readline().rstrip("\n")
+
+    names = broken_header.split(",") # 39 names right now
+    names.insert(7, "test")  # missing header at index 7 (after index 6)
+
+    assert len(names) == 40, f"Expected 40 names, got {len(names)}"
+
+    # Read the CSV using new names, skip the broken header row
+    games = pd.read_csv(path, header=None, names=names, skiprows=1)
+    
+    return games
